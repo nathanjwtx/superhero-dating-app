f584a23dea8f92a07cb84e5e056f71583d8ad433
Nathan Waterman <nathanjwtx@nathanjwtx.io>
2019-04-19 07:49:09 -0500

    Added basic CORS permissions and printed database values to screen

--- DatingApp-SPA/src/app/value/value.component.ts
+++ DatingApp-SPA/src/app/value/value.component.ts
@@ -12,9 +12,14 @@ export class ValueComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getValues();
   }
 
   getValues() {
-    this.http.get('http://localhost:5000/api/values')
+    this.http.get('http://localhost:5000/api/values').subscribe(response => {
+      this.values = response;
+    }, error => {
+      console.log(error);
+    });
   }
 }
